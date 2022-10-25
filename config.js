e58ce50ecce43befbb54d621da3c16cb81949b04
Kervan Code <samqwea31@gmail.com>
2022-10-25 19:21:04 +0400

    mod9
    mod

---
+++ config.js
@@ -0,0 +1,100 @@
+module.exports = {
+//BİR SORUNLA KARŞILAŞIRSANIZ Relly #1784//
+  "tag": "➹",
+
+  "tag2": "☨",
+  "serverid": "1033435634414657576",
+  "sesknal": "1033435634863444030",
+  //-------------------------------------------------------------------------------\/
+  "Owner": "1033700583506464800",
+  "owneer":"1033700529949388870",
+  "banhammer": "1033457305393569892",
+  "jailhammer": "1033457371818758174",
+  "transport": "1034484125454835733",
+  "mutehammer": "1033457328575500318",
+  "vmutehammer": "1033457348099973130",
+  "botkomut":"1033688859873136751",
+  //-------------------------------------------------------------------------------\/
+"vocal": "",
+"designer": "",
+"sponsor": "",
+"şair": "",
+ //-------------------------------------------------------------------------------\/
+
+  
+  "chatkanalı": "830411404942901249",
+  "unregisterlog": "830701663664865280",
+  "hgkanal": "830411377150656542",
+  "taglog" : "830411409640259614",
+  "toplantikanal": "",
+  "yetkiverlog":"830702187696750624",
+  "kurallar":"830411379776421908",
+  "ability":"",
+  "MessageLogs":"830411408771383326",
+  "voicelog": "830411412730937355",
+  "voicegiriş": "830711166975868959",
+  "voiceçıkış": "830711185979473941",
+  "voicetransfer": "830711376606658571",
+  "voiceselfmute": "830711485205839902",
+  "voiceselfdeaf": "830713363792527370",
+  "rules":"830411379776421908",
+  "rollog":"830713483100028949",
+  
+  "mutelog":"1033703013505843200",
+  "mutelogid":"1033703013505843200",
+  "mutelogtoken":"kpVv7D9u4d81J7SAnFLLCocwMrMIYob6N3UMM1eI-IiTDz5gjEwFA-ORP6B6bFAEvrut",
+
+  "vmutelog": "1033703029867810898",
+  "vmutelogid":"1033703029867810898",
+  "vmutelogtoken":"Yzq83HquGkB8ZNjEqpI37NlJWe2TXXvkFnDNx1D61yNQRRZMawI04LF6mzvT3KOYhs8c",
+
+  "banlog":"1033702927048654858",
+  "banlogid":"1033702927048654858",
+  "banlogtoken":"VBS6giNk1YG966TSolzVdupM2spq2jxnRldap_MGp3zWwi6eNfXKDm-aDDGMzwfyzaqQ",
+
+  "jaillog":"1033702971432779776",
+  "jaillogtoken":"rueMVbKi3ycp7o2TBu54uvxqvu_GhIeFJDyNSGIkk9u3KnUW5sQ30MKAe3b2YCrLmyJD",
+  "jaillogid":"1033702971432779776",
+  //-------------------------------------------------------------------------------\/
+  "booster": "830437639047086141",
+  "cezalırol": "830411349972615248",
+  "muterol": "830411350651174935",
+  "yetkilirol": "830411312986324993",
+  "kayıtcırol": "830714455980638209",
+  "katıldırol": "",
+  "staffrole":"",
+  "denemeyetkili":"830411313917853716",
+  "erkek1":"1033699762450473011",
+  "erkek2":"",
+  "erkek3":"",
+  "kadın1":"1033699950502096926",
+  "kadın2":"",
+  "kadın3":"",
+  "unregister1":"1033700689882398741",
+  "unregister2":"",
+
+  "family" : "1033700345462923334",
+
+  "şüphelihesap":"1033701254540562505",
+  "uyarılog":"1033703100915138590",
+  "responsible":"",
+  //-------------------------------------------------------------------------------\/
+  "onayemoji": "830717520943644702",
+  "redemoji": "830717573322113054",
+  "hgemoji": "<a:wio_yildiz:830717291644977163>",
+  "onayemoji1": "<a:wio_yes:830717520943644702>",
+  "redemoji2": "<a:wio_hayir:830717573322113054>",
+  "okemoji": "<a:wio_yes:830717520943644702>",
+  
+///////
+
+"streamercezalırolü": "",
+
+"streamer+18kanal1": "",
+
+"streamer+18kanal2": "",
+
+"streamercezalılog": "",
+
+////////
+};
